d8f1e848d6d89274e6fc93bfe223bad5200537d6
clement ledey <clement.ledey@gmail.com>
2015-10-13 12:05:41 +0200

    added task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'index', to: 'tasks#index'
+
+  get 'show', to: 'tasks#show'
+
+  get 'new', to: 'tasks#new'
+
+  get 'create', to: 'tasks#create'
+
+  get 'edit', to: 'tasks#edit'
+
+  get 'update', to: 'tasks#update'
+
+  get 'destroy', to: 'tasks#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
